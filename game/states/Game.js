79ec0c87b82037793287024c08ae4235d8314178
laserblue <spectramollisol@yahoo.com>
2018-06-15 12:37:00 -0600

    Update Game.js
     Line 10 missing 't' in stroke. srokeThickness: 4};
    line 13: missing right bracket.   txt.stroke = "rgba(0,0,0,0";

--- game/states/Game.js
+++ game/states/Game.js
@@ -7,7 +7,7 @@ Game.prototype = {
   },
 
   addMenuOption: function(text, callback) {
-    var optionStyle = { font: '30pt TheMinion', fill: 'white', align: 'left', stroke: 'rgba(0,0,0,0)', srokeThickness: 4};
+    var optionStyle = { font: '30pt TheMinion', fill: 'white', align: 'left', stroke: 'rgba(0,0,0,0)', strokeThickness: 4};
     var txt = game.add.text(game.world.centerX, (this.optionCount * 80) + 200, text, optionStyle);
     txt.anchor.setTo(0.5);
     txt.stroke = "rgba(0,0,0,0";
